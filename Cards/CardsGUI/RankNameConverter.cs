387a68994993cfbee230a4ce6482ab1245ae7e22
Yaofuxing <2443397691@qq.com>
2018-08-22 18:31:08 +0800

    sd

---
+++ Cards/CardsGUI/RankNameConverter.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace CardsGUI
+{
+    [ValueConversion(typeof(CH11.Rank),typeof(string))]
+    public class RankNameConverter:IValueConverter
+    {
+        public object Convert(object value,Type targetType,object parameter,System.Globalization.CultureInfo culture)
+        {
+            int source = (int)value;
+            if (source == 1 || source > 10)
+            {
+                switch (source)
+                {
+                    case 1: return "A";
+                    case 11: return "J";
+                    case 12: return "Q";
+                    case 13: return "K";
+                    default:
+                        return DependencyProperty.UnsetValue;
+                }
+            }
+            else
+                return source.ToString();
+        }
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return DependencyProperty.UnsetValue;
+        }
+    }
+}
